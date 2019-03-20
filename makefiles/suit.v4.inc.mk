2b13b088282980bf9d1c635b37285cc6e0b7f370
Alexandre Abadie <alexandre.abadie@inria.fr>
2019-03-20 21:49:25 +0100

    makefiles/suit.v4.inc.mk: refactor build system integration

--- makefiles/suit.v4.inc.mk
+++ makefiles/suit.v4.inc.mk
@@ -1,14 +1,9 @@
 #
-SUIT_COAP_BASEPATH ?= fw/$(BOARD)
-SUIT_COAP_SERVER ?= localhost
-SUIT_COAP_ROOT ?= coap://$(SUIT_COAP_SERVER)/$(SUIT_COAP_BASEPATH)
-SUIT_COAP_FSROOT ?= $(RIOTBASE)/coaproot
+SUIT_MAKEFILE ?= local
 
 #
 SUIT_MANIFEST ?= $(BINDIR_APP)-riot.suitv4.$(APP_VER).bin
-SUIT_MANIFEST_LATEST ?= $(BINDIR_APP)-riot.suitv4.latest.bin
 SUIT_MANIFEST_SIGNED ?= $(BINDIR_APP)-riot.suitv4_signed.$(APP_VER).bin
-SUIT_MANIFEST_SIGNED_LATEST ?= $(BINDIR_APP)-riot.suitv4_signed.latest.bin
 
 # Long manifest names require more buffer space when parsing
 export CFLAGS += -DSOCK_URLPATH_MAXLEN=128
@@ -20,13 +15,11 @@ SUIT_KEY ?= secret.key
 SUIT_PUB ?= public.key
 SUIT_PUB_HDR ?= public_key.h
 
-$(SUIT_KEY) $(SUIT_PUB):
-	@$(RIOTBASE)/dist/tools/suit_v4/gen_key.py
-
-$(SUIT_PUB_HDR): $(SUIT_PUB)
-	@xxd -i $(SUIT_PUB) > $@
-
-suit/genkey: $(SUIT_KEY) $(SUIT_PUB) $(SUIT_PUB_HDR)
+ifeq (local,$(SUIT_MAKEFILE))
+  include $(RIOTMAKE)/suit.v4.local.inc.mk
+else
+  include $(SUIT_MAKEFILE)
+endif
 
 $(SUIT_MANIFEST): $(SLOT0_RIOT_BIN) $(SLOT1_RIOT_BIN)
 	$(RIOTBASE)/dist/tools/suit_v4/gen_manifest.py \
@@ -42,30 +35,3 @@ $(SUIT_MANIFEST): $(SLOT0_RIOT_BIN) $(SLOT1_RIOT_BIN)
 $(SUIT_MANIFEST_SIGNED): $(SUIT_MANIFEST)
 	$(RIOTBASE)/dist/tools/suit_v4/sign-04.py \
 	  $(SUIT_KEY) $(SUIT_PUB) $< $@
-
-$(SUIT_MANIFEST_LATEST): $(SUIT_MANIFEST)
-	@ln -f -s $< $@
-
-$(SUIT_MANIFEST_SIGNED_LATEST): $(SUIT_MANIFEST_SIGNED)
-	@ln -f -s $< $@
-
-SUIT_MANIFESTS := $(SUIT_MANIFEST) \
-                  $(SUIT_MANIFEST_LATEST) \
-		  $(SUIT_MANIFEST_SIGNED) \
-		  $(SUIT_MANIFEST_SIGNED_LATEST)
-
-suit/manifest: $(SUIT_MANIFESTS)
-
-suit/publish: $(SUIT_MANIFESTS) $(SLOT0_RIOT_BIN) $(SLOT1_RIOT_BIN)
-	@mkdir -p $(SUIT_COAP_FSROOT)/$(SUIT_COAP_BASEPATH)
-	@cp -t $(SUIT_COAP_FSROOT)/$(SUIT_COAP_BASEPATH) $^
-	@for file in $^; do \
-		echo "published \"$$file\""; \
-		echo "       as \"$(SUIT_COAP_ROOT)/$$(basename $$file)\""; \
-	done
-
-suit/notify: | $(filter suit/publish, $(MAKECMDGOALS))
-	@test -n "$(SUIT_CLIENT)" || { echo "error: SUIT_CLIENT unset!"; false; }
-	aiocoap-client -m POST "coap://$(SUIT_CLIENT)/suit/trigger" \
-		--payload "$(SUIT_COAP_ROOT)/$$(basename $(SUIT_MANIFEST_SIGNED_LATEST))" && \
-		echo "Triggered $(SUIT_CLIENT) to update."
---
+++ makefiles/suit.v4.local.inc.mk
@@ -0,0 +1,44 @@
+#
+SUIT_COAP_BASEPATH ?= fw/$(BOARD)
+SUIT_COAP_SERVER ?= localhost
+SUIT_COAP_ROOT ?= coap://$(SUIT_COAP_SERVER)/$(SUIT_COAP_BASEPATH)
+SUIT_COAP_FSROOT ?= $(RIOTBASE)/coaproot
+
+#
+SUIT_MANIFEST_LATEST ?= $(BINDIR_APP)-riot.suitv4.latest.bin
+SUIT_MANIFEST_SIGNED_LATEST ?= $(BINDIR_APP)-riot.suitv4_signed.latest.bin
+
+$(SUIT_MANIFEST_LATEST): $(SUIT_MANIFEST)
+	@ln -f -s $< $@
+
+$(SUIT_MANIFEST_SIGNED_LATEST): $(SUIT_MANIFEST_SIGNED)
+	@ln -f -s $< $@
+
+SUIT_MANIFESTS := $(SUIT_MANIFEST) \
+                  $(SUIT_MANIFEST_LATEST) \
+                  $(SUIT_MANIFEST_SIGNED) \
+                  $(SUIT_MANIFEST_SIGNED_LATEST)
+
+suit/manifest: $(SUIT_MANIFESTS)
+
+suit/publish: $(SUIT_MANIFESTS) $(SLOT0_RIOT_BIN) $(SLOT1_RIOT_BIN)
+	@mkdir -p $(SUIT_COAP_FSROOT)/$(SUIT_COAP_BASEPATH)
+	@cp -t $(SUIT_COAP_FSROOT)/$(SUIT_COAP_BASEPATH) $^
+	@for file in $^; do \
+		echo "published \"$$file\""; \
+		echo "       as \"$(SUIT_COAP_ROOT)/$$(basename $$file)\""; \
+	done
+
+suit/notify: | $(filter suit/publish, $(MAKECMDGOALS))
+	@test -n "$(SUIT_CLIENT)" || { echo "error: SUIT_CLIENT unset!"; false; }
+	aiocoap-client -m POST "coap://$(SUIT_CLIENT)/suit/trigger" \
+		--payload "$(SUIT_COAP_ROOT)/$$(basename $(SUIT_MANIFEST_SIGNED_LATEST))" && \
+		echo "Triggered $(SUIT_CLIENT) to update."
+
+$(SUIT_KEY) $(SUIT_PUB):
+	@$(RIOTBASE)/dist/tools/suit_v4/gen_key.py
+
+$(SUIT_PUB_HDR): $(SUIT_PUB)
+	@xxd -i $(SUIT_PUB) > $@
+
+suit/genkey: $(SUIT_KEY) $(SUIT_PUB) $(SUIT_PUB_HDR)
