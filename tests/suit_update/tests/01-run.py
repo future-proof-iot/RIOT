868fc7543171537d3e51de74ab3f2694dc61f8d8
francisco <femolina@uc.cl>
2019-05-21 17:40:12 +0200

    tests/suit: pass tags as variables

--- tests/suit_update/tests/01-run.py
+++ tests/suit_update/tests/01-run.py
@@ -13,6 +13,7 @@ import sys
 import time
 from testrunner import run
 
+
 UPDATE_TIMEOUT = 120
 MANIFEST_TIMEOUT = 15
 
@@ -20,8 +21,11 @@ CURRENT_DIR = os.path.dirname(sys.argv[0])
 RIOT_DIR = os.path.abspath(os.path.join(CURRENT_DIR, '../../..'))
 SUIT_DIR = os.path.abspath(os.path.join(RIOT_DIR, 'dist/tools/suit/'))
 
-SUIT_LATEST_1 = 'late-1'
-SUIT_LATEST_2 = 'late-2'
+
+if 'TAGS' in os.environ:
+    TAGS = os.environ['TAGS'].split(' ')
+else:
+    TAGS = ['late-1', 'late_2']
 
 COAP_HOST = '[fd00:dead:beef::1]'
 
@@ -36,39 +40,27 @@ def notify(server_url, client_url, latest):
 def testfunc(child):
     """For one board test if specified application is updatable"""
 
-    # Intial Setup and wait for address configuration
-    time.sleep(3)
-    child.expect_exact('main(): This is RIOT!')
-    child.expect(r'inet6 addr: (?P<gladdr>[0-9a-fA-F:]+:[A-Fa-f:0-9]+)'
-                 '  scope: global  VAL')
-    client = "[{}]".format(child.match.group("gladdr").lower())
 
-    # Notify first time
-    notify(COAP_HOST, client, SUIT_LATEST_1)
-    child.expect_exact('suit_coap: trigger received')
-    child.expect_exact('suit: verifying manifest signature...')
-    child.expect(
-        r'riotboot_flashwrite: initializing update to target slot \d',
-        timeout=MANIFEST_TIMEOUT)
+    for tag in TAGS:
+        # Intial Setup and wait for address configuration
+        time.sleep(3)
+        child.expect_exact('main(): This is RIOT!')
+        child.expect(r'inet6 addr: (?P<gladdr>[0-9a-fA-F:]+:[A-Fa-f:0-9]+)'
+                    '  scope: global  VAL')
+        client = "[{}]".format(child.match.group("gladdr").lower())
+        # Intial Setup and wait for address configuration
+        notify(COAP_HOST, client, tag)
+        child.expect_exact('suit_coap: trigger received')
+        child.expect_exact('suit: verifying manifest signature...')
+        child.expect(
+            r'riotboot_flashwrite: initializing update to target slot \d',
+            timeout=MANIFEST_TIMEOUT)
 
-    # Wait for update to complete
-    child.expect_exact(
-        'riotboot_flashwrite: riotboot flashing completed successfully',
-        timeout=UPDATE_TIMEOUT)
-    child.expect_exact('main(): This is RIOT!')
+        # Wait for update to complete
+        child.expect_exact(
+            'riotboot_flashwrite: riotboot flashing completed successfully',
+            timeout=UPDATE_TIMEOUT)
 
-    # Notify second time
-    notify(COAP_HOST, client, SUIT_LATEST_2)
-    child.expect_exact('suit_coap: trigger received')
-    child.expect_exact('suit: verifying manifest signature...')
-    child.expect(
-        r'riotboot_flashwrite: initializing update to target slot \d',
-        timeout=MANIFEST_TIMEOUT)
-
-    # Wait for update to complete
-    child.expect_exact(
-        'riotboot_flashwrite: riotboot flashing completed successfully',
-        timeout=UPDATE_TIMEOUT)
     child.expect_exact('main(): This is RIOT!')
     print("TEST PASSED")
 
